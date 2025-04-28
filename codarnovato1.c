067cc8ab849ec254970bc46b7a4cee2a6ccc0269
DanPrOgRaMaCaO <dan75yz7@gmail.com>
2025-04-28 18:02:12 +0000

    novato

---
+++ codarnovato1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (){
+    int i = 1;
+
+    while (i <= 10){
+
+        if (i % 2 == 0)
+        {
+            printf("O numero é %d é par.\n", i);
+        }
+        i++;
+    }
+    
+
+
+
+
+
+
+}
